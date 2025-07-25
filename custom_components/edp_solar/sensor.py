1d8879604ac247e9b3c0d51fae05a729df13a1ed
Rodrigo Azevedo <r.mag.azevedo@gmail.com>
2025-07-25 09:17:58 +0100

    adding device structure to better organize sensors

--- custom_components/edp_solar/sensor.py
+++ custom_components/edp_solar/sensor.py
@@ -8,28 +8,28 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, entry, async_add_entities):
     """Set up EDP Solar sensors from a config entry."""
-    # Retrieve the API instance - CRITICAL FIX HERE
+    # Retrieve the API instance
     api = hass.data[DOMAIN][entry.entry_id]
-    
-    # Create sensors
+
+    # Create sensors with entry_id
     sensors = [
-        EdpSolarInstantPowerProducedSensor(api),
-        EdpSolarInstantPowerConsumedSensor(api),
-        EdpSolarInstantPowerFromGridSensor(api),
-        EdpSolarInstantPowerInjectedSensor(api),
-        EdpSolarAvailableDeviceIdsSensor(api),
-        EdpSolarHouseIdSensor(api),
-        EdpSolarUserIdSensor(api),
-        EdpSolarEnergyConsumed(api),
-        EdpSolarEnergyFromGrid(api),
-        EdpSolarEnergyInjected(api),
-        EdpSolarEnergyProduced(api),
+        EdpSolarInstantPowerProducedSensor(api, entry.entry_id),
+        EdpSolarInstantPowerConsumedSensor(api, entry.entry_id),
+        EdpSolarInstantPowerFromGridSensor(api, entry.entry_id),
+        EdpSolarInstantPowerInjectedSensor(api, entry.entry_id),
+        EdpSolarAvailableDeviceIdsSensor(api, entry.entry_id),
+        EdpSolarHouseIdSensor(api, entry.entry_id),
+        EdpSolarUserIdSensor(api, entry.entry_id),
+        EdpSolarEnergyConsumed(api, entry.entry_id),
+        EdpSolarEnergyFromGrid(api, entry.entry_id),
+        EdpSolarEnergyInjected(api, entry.entry_id),
+        EdpSolarEnergyProduced(api, entry.entry_id),
     ]
     async_add_entities(sensors)
 
     # Listen for updates from the API (triggered by MQTT callback)
     async def _update_sensors():
-        for sensor in sensors:            
+        for sensor in sensors:
             sensor.async_schedule_update_ha_state(True)
 
     entry.async_on_unload(
@@ -39,18 +39,31 @@ async def async_setup_entry(hass, entry, async_add_entities):
 class EdpSolarBaseSensor(SensorEntity):
     """Base sensor for EDP Solar."""
 
-    def __init__(self, api):
+    def __init__(self, api, entry_id):
         self.api = api
+        self._entry_id = entry_id
 
     @property
     def should_poll(self):
         return False  # Data is pushed via MQTT
+
+    @property
+    def device_info(self):
+        return {
+            "identifiers": {(DOMAIN, self._entry_id)},
+            "name": "EDP Solar",
+            "manufacturer": "EDP",
+            "model": "EDP Solar Integration",
+        }
 
 class EdpSolarInstantPowerProducedSensor(EdpSolarBaseSensor):
     _attr_name = "Instant Power Produced"
     _attr_native_unit_of_measurement = UnitOfPower.WATT
     _attr_icon = "mdi:solar-power"
     _attr_unique_id = "edp_solar_instant_power_produced"
+
+    def __init__(self, api, entry_id):
+        super().__init__(api, entry_id)
 
     @property
     def native_value(self):
@@ -62,6 +75,9 @@ class EdpSolarInstantPowerConsumedSensor(EdpSolarBaseSensor):
     _attr_icon = "mdi:flash"
     _attr_unique_id = "edp_solar_instant_power_consumed"
 
+    def __init__(self, api, entry_id):
+        super().__init__(api, entry_id)
+
     @property
     def native_value(self):
         return self.api.get_values().get("instant_power_consumed")
@@ -71,6 +87,9 @@ class EdpSolarInstantPowerInjectedSensor(EdpSolarBaseSensor):
     _attr_native_unit_of_measurement = UnitOfPower.WATT
     _attr_icon = "mdi:transmission-tower-export"
     _attr_unique_id = "edp_solar_instant_power_injected"
+
+    def __init__(self, api, entry_id):
+        super().__init__(api, entry_id)
 
     @property
     def native_value(self):
@@ -82,6 +101,9 @@ class EdpSolarInstantPowerFromGridSensor(EdpSolarBaseSensor):
     _attr_icon = "mdi:transmission-tower"
     _attr_unique_id = "edp_solar_instant_power_from_grid"
 
+    def __init__(self, api, entry_id):
+        super().__init__(api, entry_id)
+
     @property
     def native_value(self):
         return self.api.get_values().get("instant_power_from_grid")
@@ -90,6 +112,9 @@ class EdpSolarAvailableDeviceIdsSensor(EdpSolarBaseSensor):
     _attr_name = "Available Device Ids"
     _attr_icon = "mdi:devices"
     _attr_unique_id = "edp_solar_available_device_ids"
+
+    def __init__(self, api, entry_id):
+        super().__init__(api, entry_id)
 
     @property
     def native_value(self):
@@ -102,6 +127,9 @@ class EdpSolarHouseIdSensor(EdpSolarBaseSensor):
     _attr_icon = "mdi:home"
     _attr_unique_id = "edp_solar_house_id"
 
+    def __init__(self, api, entry_id):
+        super().__init__(api, entry_id)
+
     @property
     def native_value(self):
         return self.api.get_values().get("house_id")
@@ -110,6 +138,9 @@ class EdpSolarUserIdSensor(EdpSolarBaseSensor):
     _attr_name = "User Id"
     _attr_icon = "mdi:account"
     _attr_unique_id = "edp_solar_user_id"
+
+    def __init__(self, api, entry_id):
+        super().__init__(api, entry_id)
 
     @property
     def native_value(self):
@@ -120,6 +151,9 @@ class EdpSolarEnergyConsumed(EdpSolarBaseSensor):
     _attr_native_unit_of_measurement = UnitOfEnergy.WATT_HOUR
     _attr_icon = "mdi:flash"
     _attr_unique_id = "edp_solar_energy_consumed"
+
+    def __init__(self, api, entry_id):
+        super().__init__(api, entry_id)
 
     @property
     def native_value(self):
@@ -132,6 +166,9 @@ class EdpSolarEnergyFromGrid(EdpSolarBaseSensor):
     _attr_icon = "mdi:transmission-tower"
     _attr_unique_id = "edp_solar_energy_from_grid"
 
+    def __init__(self, api, entry_id):
+        super().__init__(api, entry_id)
+
     @property
     def native_value(self):
         ws = self.api.get_values().get("energy_from_grid")
@@ -140,8 +177,11 @@ class EdpSolarEnergyFromGrid(EdpSolarBaseSensor):
 class EdpSolarEnergyInjected(EdpSolarBaseSensor):
     _attr_name = "Energy Injected"
     _attr_native_unit_of_measurement = UnitOfEnergy.WATT_HOUR
-    _attr_icon =  "mdi:transmission-tower-export"
+    _attr_icon = "mdi:transmission-tower-export"
     _attr_unique_id = "edp_solar_energy_injected"
+
+    def __init__(self, api, entry_id):
+        super().__init__(api, entry_id)
 
     @property
     def native_value(self):
@@ -153,6 +193,9 @@ class EdpSolarEnergyProduced(EdpSolarBaseSensor):
     _attr_native_unit_of_measurement = UnitOfEnergy.WATT_HOUR
     _attr_icon = "mdi:solar-power"
     _attr_unique_id = "edp_solar_energy_produced"
+
+    def __init__(self, api, entry_id):
+        super().__init__(api, entry_id)
 
     @property
     def native_value(self):
